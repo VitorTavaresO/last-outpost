3858513f78c7b06b277029db3e957cfeba3f6b19
VitorTavaresO <vitortavares.o55@gmail.com>
2023-09-11 13:51:31 -0300

    Start

---
+++ target/debug/deps/reference_fight.d
@@ -0,0 +1,5 @@
+C:\Codes\reference_fight\target\debug\deps\reference_fight.exe: src\main.rs
+
+C:\Codes\reference_fight\target\debug\deps\reference_fight.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/reference_fight.d
@@ -0,0 +1,1 @@
+C:\Codes\reference_fight\target\debug\reference_fight.exe: C:\Codes\reference_fight\src\main.rs
