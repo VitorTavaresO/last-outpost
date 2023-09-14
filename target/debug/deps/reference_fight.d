44cb15ca16fd9c41522e193364e399ef2be9b218
VitorTavaresO <vitortavares.o55@gmail.com>
2023-09-14 20:09:34 -0300

    Hello Cpp

--- target/debug/deps/reference_fight.d
+++
@@ -1,5 +0,0 @@
-C:\Codes\reference_fight\target\debug\deps\reference_fight.exe: src\main.rs
-
-C:\Codes\reference_fight\target\debug\deps\reference_fight.d: src\main.rs
-
-src\main.rs:
--- target/debug/reference_fight.d
+++
@@ -1,1 +0,0 @@
-C:\Codes\reference_fight\target\debug\reference_fight.exe: C:\Codes\reference_fight\src\main.rs
