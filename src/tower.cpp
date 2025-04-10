e670f7b18d7cb3f56715e7dd9e55f11238ca85a2
earibeiro <eduardoribeiro.advg@gmail.com>
2025-04-09 21:51:21 -0300

    Fix: Fixed a minor inconvenience

--- src/tower.cpp
+++ src/tower.cpp
@@ -1,50 +1,15 @@
-#ifndef LAST_OUTPOST_TOWER_H
-#define LAST_OUTPOST_TOWER_H
-
-#include <my-lib/matrix.h>
-#include <last-outpost/object.h>
-#include <last-outpost/types.h>
+#include <last-outpost/tower.h>
+#include <last-outpost/graphics.h>
+#include <cmath>
 
 namespace Game
 {
-	class Tower : public Object
+	void Tower::update(float deltaTime)
 	{
-	private:
-		float range;
-		float damage;
+	}
 
-	public:
-		Tower(float range = 0.0f, float damage = 0.0f, SDL_Color color = {0, 0, 255, 255})
-			: range(range), damage(damage)
-		{
-			this->setType(ObjectType::Tower);
-			this->setColor(color);
-			this->setSize(1, 1);
-		}
-
-		void setRange(int range)
-		{
-			this->range = range;
-		}
-
-		float getRange() const
-		{
-			return this->range;
-		}
-
-		void setDamage(float damage)
-		{
-			this->damage = damage;
-		}
-
-		float getDamage() const
-		{
-			return this->damage;
-		}
-
-		void update(float deltaTime) override;
-		void render(Graphics &graphics, float deltaTime) const override;
-	};
+	void Tower::render(Graphics &graphics, float deltaTime) const
+	{
+		graphics.drawRect(getPosition(), getSize(), getColor());
+	}
 }
-
-#endif
