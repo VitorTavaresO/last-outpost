fbea74b6353db2bdfdf99edacf821589a7f3f1e5
earibeiro <eduardoribeiro.advg@gmail.com>
2025-04-16 14:24:49 -0300

    The tower logic mechanism was changed in order to use the projectile

--- include/last-outpost/tower.h
+++ include/last-outpost/tower.h
@@ -4,18 +4,23 @@
 #include <my-lib/matrix.h>
 #include <last-outpost/object.h>
 #include <last-outpost/types.h>
+#include <last-outpost/projectil.h>
+#include <memory>
 
 namespace Game
 {
+	class Enemy;
 	class Tower : public Object
 	{
 	private:
 		float range;
 		float damage;
+		float lastFireTime;
+		float fireRate;
 
 	public:
 		Tower(float range = 0.0f, float damage = 0.0f, SDL_Color color = {0, 0, 255, 255})
-			: range(range), damage(damage)
+			: range(range), damage(damage), lastFireTime(0), fireRate(1.0f)
 		{
 			this->setType(ObjectType::Tower);
 			this->setColor(color);
@@ -26,7 +31,6 @@ namespace Game
 		{
 			this->range = range;
 		}
-
 		float getRange() const
 		{
 			return this->range;
@@ -36,15 +40,25 @@ namespace Game
 		{
 			this->damage = damage;
 		}
-
 		float getDamage() const
 		{
 			return this->damage;
 		}
 
+		void setFireRate(float rate)
+		{
+			this->fireRate = rate;
+		}
+		float getFireRate() const
+		{
+			return this->fireRate;
+		}
+
 		void update(float deltaTime) override;
 		void render(Graphics &graphics, float deltaTime) const override;
 		bool isWithinRange(const Vector &targetPosition) const;
+
+		std::unique_ptr<Projectil> tryFireAt(Enemy *enemy, float currentTime);
 	};
 }
 
