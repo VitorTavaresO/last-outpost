92bc99c778e427733dc598ce0fa26089e0ed7dcd
VitorTavaresO <vitortavares.o55@gmail.com>
2025-04-30 19:48:19 -0300

    Removed Logic from Main and Passed to Game_control.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,17 @@
-#include <SDL.h>
-#include <iostream>
-#include <utility>
-#include <last-outpost/globals.h>
-#include <last-outpost/game_world.h>
-#include <last-outpost/level.h>
-#include <last-outpost/globals.h>
+#include <last-outpost/game_control.h>
 
 namespace Game
 {
 	int main(int argc, char **argv)
 	{
-		if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		GameControl gameControl;
+
+		if (!gameControl.initialize())
 		{
-			std::cerr << "Error on start SDL: " << SDL_GetError() << std::endl;
 			return 1;
 		}
 
-		SDL_Window *window = SDL_CreateWindow("View Map", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == nullptr)
-		{
-			std::cerr << "Error on Window Creation " << SDL_GetError() << std::endl;
-			SDL_Quit();
-			return 1;
-		}
-
-		SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-		if (renderer == nullptr)
-		{
-			std::cerr << "Error on render creation: " << SDL_GetError() << std::endl;
-			SDL_DestroyWindow(window);
-			SDL_Quit();
-			return 1;
-		}
-
-		Map map(TILES_X, TILES_Y, rawStringMap);
-		auto path = map.extractPath();
-
-		std::vector<Enemy> enemyTypes = {Enemy(100, 20, 1.0f, "Fireball", path),
-										 Enemy(150, 30, 5.0f, "Ice Spike", path),
-										 Enemy(200, 40, 10.0f, "Lightning Bolt", path)};
-
-		Level LEVEL_1(rawStringMap, std::move(enemyTypes), 100);
-		GameWorld gameWorld(renderer, SCREEN_WIDTH, SCREEN_HEIGHT, std::move(LEVEL_1));
-		gameWorld.run();
-
-		SDL_DestroyRenderer(renderer);
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-
+		gameControl.run();
 		return 0;
 	}
 }
